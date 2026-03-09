0b71ed18ff1ab1dff17a6a7ef02aef51865792d1
Sadhana kushwaha <kushwahasadhana317@gmail.com>
2026-03-09 12:32:12 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,12 @@
+function add(value) {
+    document.getElementById("display").value += value;
+}
+
+function calculate() {
+    document.getElementById("display").value =
+        eval(document.getElementById("display").value);
+}
+
+function clearDisplay() {
+    document.getElementById("display").value = "";
+}
